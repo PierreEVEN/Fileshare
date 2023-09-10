4a63ee0aa44844bfba535af79522d63796081ed1
Pierre EVEN <peven@unistra.fr>
2023-09-10 10:06:36 +0200

    fixed some bugs

--- public/scripts/file_display/mime_utils.js
+++ public/scripts/file_display/mime_utils.js
@@ -1,5 +1,19 @@
 const Handlebars = require('handlebars');
 
+function get_mime_alias(mimetype) {
+    switch (mimetype) {
+        case 'application/x-zip':
+        case 'application/x-zip-compressed':
+            return 'application/zip';
+        case 'application/x-javascript':
+            return 'application/javascript';
+        case 'text/jade':
+        case 'text/pug':
+            return 'text/template';
+    }
+    return mimetype;
+}
+
 function is_valid(mimetype) {
     if (!mimetype)
         return false;
@@ -18,7 +32,7 @@ function get_mime_icon_path(mimetype) {
     if (!is_valid(mimetype))
         return '/images/icons/no-mime-icon.png';
 
-    const [mime_left, mime_right] = mimetype.split('/');
+    const [mime_left, mime_right] = get_mime_alias(mimetype).split('/');
 
     const mime_icons = Handlebars.get_mime_icons();
     const mime_category = mime_icons[mime_left];
