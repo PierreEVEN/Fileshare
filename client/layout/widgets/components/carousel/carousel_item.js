afc58966ae046d225d8f942f0b258b90fd8dc5ab
Pierre EVEN <evenpierre@orange.fr>
2024-07-24 12:53:46 +0200

    wip zoom

--- client/layout/widgets/components/carousel/carousel_item.js
+++ client/layout/widgets/components/carousel/carousel_item.js
@@ -1,9 +1,31 @@
 const carousel_item_hbs = require('./carousel_item.hbs')
 
+function clamp(s, a, b) { return s < a ? a : s > b ? b : s; }
+
 class CarouselItem {
     constructor(container, item) {
         container.innerHTML = '';
-        container.append(carousel_item_hbs({item: item}));
+        /**
+         * @type {HTMLElement}
+         */
+        const visual = carousel_item_hbs({item: item});
+        container.append(visual);
+
+        this.scale = 1;
+        this.translationX = 0;
+        this.translationY = 0;
+
+        visual.addEventListener("wheel", e=>{
+                if(e.ctrlKey) {
+                    e.stopPropagation();
+                    const zoom = -clamp(e.deltaY, -29, 29) / 30 + 1;
+                    this.scale = clamp(this.scale * zoom, 1, 50);
+                    const offsetX = e.clientX - window.width / 2;
+                    const offsetY = e.clientY - window.height / 2;  
+                    visual.style.transform = `scale(${this.scale}) translate(${this.translationX}px, ${this.translationY}px)`;
+                    e.preventDefault();
+                }
+            });
     }
 }
 export {CarouselItem}
